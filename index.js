ebd960486d8e545ded23f42b09dc59663d3f331f
Aman Mehrat <aman.preparationcom@gmail.com>
2020-01-22 05:33:07 +0530

    Project Setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({
+    firstName: "AMAN",
+    lastName: "Mehrat"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is listening");
+});
